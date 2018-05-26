718b70ae80909f063bfcadd880775ccae1257cf9
RamonValenzuela <ramon.v@ou.edu>
2018-05-25 21:13:11 -0500

    Commiting master code

---
+++ app/src/main/java/us/onyxia/linkerv2/MainActivity.java
@@ -0,0 +1,13 @@
+package us.onyxia.linkerv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
